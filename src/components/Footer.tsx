8e3ab9070978046ad983b1ee598ecf6f301f1307
Naveen Patidar <naveenpatidar951@gmail.com>
2026-04-03 18:05:04 +0530

    full update

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <footer className="py-8 px-6 bg-primary text-primary-foreground/60 text-center text-sm font-body">
-    © {new Date().getFullYear()} Your Name. All rights reserved.
-  </footer>
-);
-
-export default Footer;
